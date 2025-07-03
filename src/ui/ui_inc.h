38bb61113eed13777c58750fa41972d297482dec
mohammadA19 <48352583+mohammadA19@users.noreply.github.com>
2025-07-04 00:45:26 +0300

    ui_inc: port to C++

--- src/ui/ui_inc.h
+++ src/ui/ui_inc.h
@@ -1,10 +1,7 @@
 // Copyright (c) Epic Games Tools
 // Licensed under the MIT license (https://opensource.org/license/mit/)
 
-#ifndef UI_INC_H
-#define UI_INC_H
+#pragma once
 
 #include "ui_core.h"
 #include "ui_basic_widgets.h"
-
-#endif // UI_INC_H
