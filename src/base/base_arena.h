09838956bf3f30ad49345a2b15bc5e69880e9d17
mohammadA19 <48352583+mohammadA19@users.noreply.github.com>
2025-07-09 07:40:16 +0300

    base_arena: Remove forward declarations

--- src/base/base_arena.h
+++ src/base/base_arena.h
@@ -1,9 +1,6 @@
 // Copyright (c) Epic Games Tools
 // Licensed under the MIT license (https://opensource.org/license/mit/)
 
-#ifndef BASE_ARENA_H
-#define BASE_ARENA_H
-
 ////////////////////////////////
 //~ rjf: Constants
 
@@ -19,7 +16,6 @@ enum
   ArenaFlag_LargePages = (1<<1),
 };
 
-typedef struct ArenaParams ArenaParams;
 struct ArenaParams
 {
   ArenaFlags flags;
@@ -30,7 +26,6 @@ struct ArenaParams
   int allocation_site_line;
 };
 
-typedef struct Arena Arena;
 struct Arena
 {
   Arena *prev;    // previous arena in chain
@@ -51,7 +46,6 @@ struct Arena
 };
 StaticAssert(sizeof(Arena) <= ARENA_HEADER_SIZE, arena_header_size_check);
 
-typedef struct Temp Temp;
 struct Temp
 {
   Arena *arena;
@@ -69,27 +63,10 @@ global ArenaFlags arena_default_flags = 0;
 //~ rjf: Arena Functions
 
 //- rjf: arena creation/destruction
-internal Arena *arena_alloc_(ArenaParams *params);
 #define arena_alloc(...) arena_alloc_(&(ArenaParams){.reserve_size = arena_default_reserve_size, .commit_size = arena_default_commit_size, .flags = arena_default_flags, .allocation_site_file = __FILE__, .allocation_site_line = __LINE__, __VA_ARGS__})
-internal void arena_release(Arena *arena);
-
-//- rjf: arena push/pop/pos core functions
-internal void *arena_push(Arena *arena, U64 size, U64 align);
-internal U64   arena_pos(Arena *arena);
-internal void  arena_pop_to(Arena *arena, U64 pos);
-
-//- rjf: arena push/pop helpers
-internal void arena_clear(Arena *arena);
-internal void arena_pop(Arena *arena, U64 amt);
-
-//- rjf: temporary arena scopes
-internal Temp temp_begin(Arena *arena);
-internal void temp_end(Temp temp);
 
 //- rjf: push helper macros
 #define push_array_no_zero_aligned(a, T, c, align) (T *)arena_push((a), sizeof(T)*(c), (align))
 #define push_array_aligned(a, T, c, align) (T *)MemoryZero(push_array_no_zero_aligned(a, T, c, align), sizeof(T)*(c))
 #define push_array_no_zero(a, T, c) push_array_no_zero_aligned(a, T, c, max(8, align_of(T)))
 #define push_array(a, T, c) push_array_aligned(a, T, c, max(8, align_of(T)))
-
-#endif // BASE_ARENA_H
